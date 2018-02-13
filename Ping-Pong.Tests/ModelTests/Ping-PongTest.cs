8ee8ba7c7a8397f76bda8fcfe1f8ab24e5bf68b5
Faiza Husain and Nanette Girzi <nanette@>
2018-02-12 16:12:10 -0800

    Add logic and test methods to Ping-Pong.cs and Ping-PongTest.cs

--- Ping-Pong.Tests/ModelTests/Ping-PongTest.cs
+++ Ping-Pong.Tests/ModelTests/Ping-PongTest.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PingPong;
+using PingPong.Models;
 using System.Collections.Generic;
 using System;
 
@@ -8,15 +8,55 @@ namespace PingPong.Tests
     [TestClass]
     public class PingPongTest
     {
-        // public void Dispose()
+        //NO LONGER NEEDEDSINCE MAIN FUNCTION HAS BEEN MODIFIED FOR PING PONG
+        // [TestMethod]
+        // public void IsPingPongGenerator_Prints1_True()
         // {
-        //     PingPongGenerator.ClearAll();
+        //     PingPongGenerator testPingPongGenerator = new PingPongGenerator();
+        //     Assert.AreEqual("1", testPingPongGenerator.IsPingPongGenerator(1));
         // }
+        //
+        // [TestMethod]
+        // public void IsPingPongGenerator_Prints12_True()
+        // {
+        //     PingPongGenerator testPingPongGenerator = new PingPongGenerator();
+        //     Assert.AreEqual("2", testPingPongGenerator.IsPingPongGenerator(2));
+        // }
+
         [TestMethod]
-        public void IsPingPongGenerator_PrintsNumbers0ToUserInput_True()
+        public void IsPingPongGenerator_ReturnPing_True()
         {
+            int number = 6;
             PingPongGenerator testPingPongGenerator = new PingPongGenerator();
-            Assert.AreEqual(true, testPingPongGenerator.IsPingPongGenerator(20));
+            Assert.AreEqual(true,testPingPongGenerator.IfPing(number));
         }
+
+        [TestMethod]
+        public void IsPingPongGenerator_ReturnPong_True()
+        {
+            int number = 10;
+            PingPongGenerator testPingPongGenerator = new PingPongGenerator();
+            Assert.AreEqual(true, testPingPongGenerator.IfPong(number));
+        }
+
+        [TestMethod]
+        public void IsPingPongGenerator_ReturnPingPong_True()
+        {
+            int number = 15;
+            PingPongGenerator testPingPongGenerator = new PingPongGenerator();
+            Assert.AreEqual(true, testPingPongGenerator.IfPingPong(number));
+        }
+
+        [TestMethod]
+        public void IsPingPongGenerator_ReturnPingPongList_True()
+        {
+            int userNumber = 15;
+            PingPongGenerator testPingPongGenerator = new PingPongGenerator();
+            List<string> testOutput = testPingPongGenerator.PingPongString(userNumber);
+            //testOutput.ForEach(i => Console.WriteLine("{0}\t", i));
+            List<string> result = new List<string>{"1", "2", "Ping", "4", "Pong", "Ping", "7", "8", "Ping", "Pong", "11", "Ping", "13", "14", "PingPong"};
+            CollectionAssert.AreEqual(result, testOutput);
+        }
+
     }
 }
--- Ping-Pong/Models/Ping-Pong.cs
+++ Ping-Pong/Models/Ping-Pong.cs
@@ -1,15 +1,70 @@
 using System.Collections.Generic;
 using System;
 
-namespace PingPong
+namespace PingPong.Models
 {
     public class PingPongGenerator
     {
-        public bool IsPingPongGenerator(int number)
+
+        public List<string> PingPongString(int number)
         {
-            //code here
-            return false;
+            List<string> pingPongList = new List<string>{};
+            for (int i = 1; i <= number; i++)
+            {
+                if (IfPingPong(i))
+                {
+                    pingPongList.Add("PingPong");
+                }
+                else if (IfPong(i))
+                {
+                    pingPongList.Add("Pong");
+                }
+                else if (IfPing(i))
+                {
+                    pingPongList.Add("Ping");
+                }
+                else
+                {
+                    pingPongList.Add(i.ToString());
+                }
+            }
+            return pingPongList;
         }
 
+        public bool IfPing(int number)
+        {
+            if (number % 3 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IfPong(int number)
+        {
+            if (number % 5 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IfPingPong(int number)
+        {
+            if ((number % 5 == 0) && (number % 3 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
